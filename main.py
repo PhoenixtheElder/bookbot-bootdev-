12e8aee75ec596132092c6aee80ca09509068d3f
Phoenix210 <Phoenix210@gmail.com>
2025-03-25 12:33:01 +0100

    main.py zählt wörter

--- main.py
+++ main.py
@@ -4,9 +4,20 @@ def get_book_text(filepath):
         book_text = f.read()
     return book_text
 
-def main():
-    text = get_book_text("./books/frankenstein.txt")
-    print(text)
-    return 0
+def count_words(analyse):
+    wordlist = []
+    with open(analyse) as f:
+        book_text = f.read()
+    wordlist = book_text.split()
+    return len(wordlist) 
 
+
+
+
+
+def main():
+    # text = get_book_text("./books/frankenstein.txt")
+    number_of_words = count_words("./books/frankenstein.txt")
+    print(number_of_words, "words found in the document")
+    
 main()
