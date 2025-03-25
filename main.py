3c1e9f5ebc56192e298669f8401ae1dd2fa947c6
Phoenix210 <Phoenix210@gmail.com>
2025-03-25 16:11:34 +0100

    main.py nimmt jetzt dynamisch den Dateipfad

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from stats import *
-
+import sys
 
 def main():
-    text = read_book("./books/frankenstein.txt")
+    
+    #Eingabe des Pfades des zu analysierenden Textes
+    script = sys.argv[0] #dies sollte immer "main.py" sein
+    
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+    #Fehlermeldung bei zu wenig Parametern (müssen 2 sein)
+    else:
+        print("Usage: python3 main.py <path_to_book>") 
+        sys.exit(1)
+
+    
+
+    
+    #Festlegen der Variablen anhand des Textes
+    text = read_book(filepath)
     number_of_words = count_words(text)
     chars_in_text = count_all_chars(text)
     sorted_chars = dict_to_list(chars_in_text)
 
 
-    #printing all those fancy statistics 
+    #Ausgabe der Textstatistik
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print("Analyzing book found at", filepath, "...")
     print("----------- Word Count ----------")
     print(f"Found {number_of_words} total words")
     print("--------- Character Count -------")
@@ -19,7 +34,7 @@ def main():
     for char_dict in sorted_chars :
         char = char_dict["char"]
         count = char_dict["count"]
-        if char.isalpha():  # Only print alphabetical characters
+        if char.isalpha():  # isalpha prüft ob der char ein Buchstabe ist 
             print(f"{char}: {count}")
 
     print("============= END ===============")
