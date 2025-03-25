08818e39d8d6f093babe1c16c655ed2cd8043539
Phoenix210 <Phoenix210@gmail.com>
2025-03-25 15:33:06 +0100

    stats.py und main.py updated damit die Auswertung erfolgt und schick aussieht

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from stats import count_all_chars, count_words, read_book
+from stats import *
+
 
 def main():
     text = read_book("./books/frankenstein.txt")
     number_of_words = count_words(text)
     chars_in_text = count_all_chars(text)
+    sorted_chars = dict_to_list(chars_in_text)
 
-    print(number_of_words, "words found in the document")
-    print(chars_in_text)
+
+    #printing all those fancy statistics 
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {number_of_words} total words")
+    print("--------- Character Count -------")
+
+    for char_dict in sorted_chars :
+        char = char_dict["char"]
+        count = char_dict["count"]
+        if char.isalpha():  # Only print alphabetical characters
+            print(f"{char}: {count}")
+
+    print("============= END ===============")
+
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+#STATS.PY
+
+
 #liest den Text aus der Datei und ihn als string zurück
 def read_book(filepath):
     with open(filepath) as f:
@@ -16,3 +19,16 @@ def count_all_chars(text):
     for char in text.lower():
         charlist[char] = charlist.get(char, 0 ) + 1
     return charlist
+
+#macht aus den gesammelten Daten von "count all chars" eine liste
+def dict_to_list(chars_in_text):
+    char_list = []
+    for char, count in chars_in_text.items():
+        char_list.append({"char" : char, "count" : count})
+    
+    #Hilfsfunktion für die geoordnete Liste
+    def sort_on(dict):
+        return dict["count"]
+    
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
